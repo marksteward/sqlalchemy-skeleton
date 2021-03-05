634f8f849f26ba113c1fb98f56453af503b1f38b
Mark Steward <marksteward@gmail.com>
2021-03-05 03:29:41 +0000

    Switch to postgres

--- Makefile
+++ Makefile
@@ -1,8 +1,21 @@
-.PHONY: run build
+.PHONY: compose copy down newdb sqlite
 
-run:
-	docker run --rm -ti -v $$PWD:/app:ro sqlalchemy-skeleton
+compose:
+	docker-compose build
+	docker-compose up -d
+	docker-compose exec main bash ||:
+	docker-compose down
 
-build:
-	docker build -t sqlalchemy-skeleton .
+copy:
+	docker cp sqlalchemy-skeleton_main_1:/app/main.py app/
+
+down:
+	docker-compose down
+
+newdb:
+	rm -rf pg-data
+
+sqlite:
+	docker build -t sqlalchemy-skeleton app
+	docker run --rm -ti sqlalchemy-skeleton
 
--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, Boolean
+from sqlalchemy.dialects.postgresql import JSON, JSONB, TSVECTOR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+from os import environ
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+# In docker-compose, DATABASE_URL will point to postgres
+url = environ.get('DATABASE_URL', 'sqlite:///:memory:')
+engine = create_engine(url, echo=True)
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -19,12 +23,27 @@ class User(Base):
     active = Column(Boolean)
     fullname = Column(String)
     nickname = Column(String)
+    vec1 = Column(TSVECTOR)
+    vec2 = Column(TSVECTOR)
+    test = Column(String)
+    data = Column(JSON)
+    datab = Column(JSONB)
 #    group_id = Column(Integer, ForeignKey('Group.id'))
 
 #    group = relationship('Group', primaryjoin='foreign(User.group_id) == Group.id')
 #    group = relationship('Group', back_populates='users')
     groups = relationship('Group', secondary='user_group')
     #, foreign_keys='[UserGroup.user_id, UserGroup.group_id]')
+
+@event.listens_for(User.fullname, 'set')
+def fullname_set(target, value, oldvalue, initiator):
+    print(target, value, oldvalue, initiator)
+    import pdb;pdb.set_trace()
+
+#@event.listens_for(User, 'after_flush')
+#def user_flushed(**kwargs):
+#  print(kwargs)
+#  import pdb;pdb.set_trace()
 
 '''
 users = Table('users', metadata,
